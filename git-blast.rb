463d4ab3c72c859ef5bb6861415a14ca1dc25fc0
David Wolever <david@wolever.net>
2019-01-21 14:56:16 -0500

    Bump version

--- git-blast.rb
+++ git-blast.rb
@@ -5,8 +5,8 @@
 class GitBlast < Formula
   desc "git-blast: show git branches sorted by last commit date Raw"
   homepage "https://github.com/wolever/git-blast/"
-  url "https://github.com/wolever/git-blast/archive/v0.2.tar.gz"
-  sha256 "1b0fc43819e77bd935ad8abb6425ad0456eca999133375d38cc3ce171d6fc811"
+  url "https://github.com/wolever/git-blast/archive/v0.3.tar.gz"
+  sha256 "0634b37bc36e13e12fdda3166b8fd95d4124affea14deabbb9ee2af01e04e11e"
 
   def install
     bin.install "./git-blast"
